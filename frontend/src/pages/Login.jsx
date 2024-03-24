275d19b41f797aa1266843e09162fae626efadeb
rowancape <niklasmk.pihlajaniemi@gmail.com>
2024-03-24 04:27:10 +0200

    Nvm. Now login works!
    Login did not work on browsers in the last commit because of cookie settings (the browsers did not have instructions to save cross-site cookies). Modified cors and cookie settings as well as making requests from axios with withCredentials set to true fixed the issue.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,187 +1,45 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import AlreadyLoggedIn from "../components/loginPage/AlreadyLoggedIn";
+import SignUpOrLogIn from "../components/loginPage/SignUpOrLogIn";
 import "../index.css";
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [isSignUp, setIsSignUp] = useState(false);
-  const [isError, setIsError] = useState(false);
+  const [effectOnLogin, setEffectOnLogin] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // > Sign up and login HTTP requests in the same function
-  const Submit = async () => {
+  // > Runs every time the page mounts or the status of effectOnLogin changes.
+  useEffect(() => {
+    getLoginStatus();
+  }, [effectOnLogin]);
+
+  // > Gets login status of current client and sets isLoggedIn accordingly.
+  const getLoginStatus = async () => {
     try {
-      const response = await axios.post(
-        isSignUp
-          ? "http://localhost:3000/users/signup"
-          : "http://localhost:3000/users/login",
-        {
-          username,
-          password,
-        }
-      );
-      // > Remove error messages if submit is successful.
-      const errorMessageContainerElement = document.getElementById(
-        "submitErrorMessages"
-      );
-      if (errorMessageContainerElement) {
-        removePreviousMessages(errorMessageContainerElement);
-      }
-      // > Switch to login if the user just created an account successfully.
-      // > Display login success message if user logged in successfully.
-      if (isSignUp) {
-        toggleSignUp();
-        const successH3 = document.getElementById("submitSuccessMessage");
-        successH3.textContent = "User created! You can now log in.";
+      const response = await axios.get("http://localhost:3000/users/status", {
+        withCredentials: true
+      });
+      if (response.data === true) {
+        setIsLoggedIn(true);
       } else {
-        const successH3 = document.getElementById("submitSuccessMessage");
-        successH3.textContent = "Login was successful!";
+        setIsLoggedIn(false);
       }
-      setIsError(false);
     } catch (err) {
-      // > Render error response messages if they are present.
-      if (err.response && Array.isArray(err.response.data)) {
-        const errorMessages = err.response.data.map((error) => error.msg);
-        renderErrorMessages(errorMessages);
-        setIsError(true);
-      } else {
-        console.log("Something went wrong");
-        setIsError(true);
-      }
+      console.log(err);
     }
   };
 
-  // > Remove previous error messages and render new ones if they are present.
-  const renderErrorMessages = (errorMessages) => {
-    const errorMessageContainerElement = document.getElementById(
-      "submitErrorMessages"
-    );
-    if (errorMessageContainerElement) {
-      removePreviousMessages(errorMessageContainerElement);
-      renderNewErrorMessages(errorMessageContainerElement, errorMessages);
-    }
+  // > Changes the status of effectOnLogin when called from child component.
+  // > Passed in as a prop to the child and called on successful login.
+  const reloadParent = () => {
+    setEffectOnLogin((a) => !a);
   };
 
-  // > Removes any previous error or success messages.
-  const removePreviousMessages = (errorMessageContainerElement) => {
-    const successH3 = document.getElementById("submitSuccessMessage");
-    successH3.textContent = "";
-    while (errorMessageContainerElement.firstChild) {
-      errorMessageContainerElement.removeChild(
-        errorMessageContainerElement.firstChild
-      );
-    }
-  };
-
-  // > Renders new error messages.
-  const renderNewErrorMessages = (
-    errorMessageContainerElement,
-    errorMessages
-  ) => {
-    for (let i = 0; i < errorMessages.length && i < 3; i++) {
-      const errorMessageElement = document.createElement("p");
-      errorMessageElement.textContent = "\u{1F6C8} " + errorMessages[i];
-      errorMessageContainerElement.appendChild(errorMessageElement);
-    }
-  };
-
-  // > Handles the clicking of the submit button.
-  const handleSubmit = () => {
-    Submit();
-  };
-
-  // > Keeps track of whether the user is on the sign up screen or the log in screen.
-  const toggleSignUp = () => {
-    setIsSignUp(!isSignUp);
-  };
-
-  // > These two functions set the username and password variables when input is changed by user.
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-    setIsError(false);
-  };
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-    setIsError(false);
-  };
-
-  return (
-    <>
-      <div className="loginPageContainer">
-        <div className="loginBox">
-          <div className="loginHeaderTextContainer">
-            <h1 className="loginHeaderText">
-              {isSignUp ? "Create account" : "Log in to your account"}
-            </h1>
-          </div>
-          <div className="loginPageUserNameAndPassContainer">
-            <div className="loginPageUserNameContainer">
-              <input
-                type="text"
-                placeholder="Username"
-                onChange={handleUsernameChange}
-                className={
-                  isError
-                    ? "errorInputFields usernameInputField"
-                    : "inputFields usernameInputField"
-                }
-              />
-            </div>
-            <div className="loginPageUserPassContainer">
-              <input
-                type="text"
-                placeholder="Password"
-                onChange={handlePasswordChange}
-                className={
-                  isError
-                    ? "errorInputFields passwordInputField"
-                    : "inputFields passwordInputField"
-                }
-              />
-            </div>
-          </div>
-          <div
-            id="submitErrorMessages"
-            className="submitMsgs submitErrorMessagesContainer"
-          ></div>
-          <div className="submitMsgs submitSuccessMessagesContainer">
-            <h3 id="submitSuccessMessage"></h3>
-          </div>
-          <div className="submitButtonContainer">
-            <button
-              type="button"
-              className={
-                isSignUp
-                  ? "submitButton signupButton"
-                  : "submitButton loginButton"
-              }
-              onClick={handleSubmit}
-            >
-              {isSignUp ? "Create account" : "Log in"}
-            </button>
-          </div>
-          <div className="loginSeparatorContainer">
-            <hr className="loginSeparator" />
-          </div>
-          <div className="loginNoAccountTextContainer">
-            <h2 className="loginNoAccountText">
-              {isSignUp
-                ? "Already have an account?"
-                : "Don't have an account yet?"}
-            </h2>
-          </div>
-          <div className="signUpToggleButtonContainer">
-            <button
-              className="signUpToggleButton"
-              type="button"
-              onClick={toggleSignUp}
-            >
-              {isSignUp ? "Log in" : "Sign up!"}
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  // > Either renders SignUpOrLogIn or AlreadyLoggedIn if user is logged in.
+  if (isLoggedIn === true) {
+    return <AlreadyLoggedIn />;
+  } else {
+    return <SignUpOrLogIn reloadParent={reloadParent} />;
+  }
 }
 export default Login;
