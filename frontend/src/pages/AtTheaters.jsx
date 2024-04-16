2d07bd7060f375613e58f2b6a7673497814a0406
Jellonaa <t2puja00@students.oamk.fi>
2024-04-16 16:16:32 +0300

    at theaters city selection

--- frontend/src/pages/AtTheaters.jsx
+++ frontend/src/pages/AtTheaters.jsx
@@ -2,14 +2,24 @@ import "../index.css";
 import React, { useState } from "react";
 import axios from "axios";
 import { parseString } from "react-native-xml2js";
-import { useEffect } from "react";
 
 function AtTheaters() {
   const [atThreaters, setAtTheaters] = useState([]);
+  const [city, setCity] = useState("");
 
-  async function getXml(){
+  const handleSelectChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  function handleButton() {
+    if (city != ""){
+      getXml(city);
+    }
+  };
+
+  async function getXml(city){
     try {
-        const result = await axios.get('https://www.finnkino.fi/xml/Schedule/?area=1018');
+        const result = await axios.get('https://www.finnkino.fi/xml/Schedule/?area=' + city);
 
         parseString(result.data, (err, parsedJson) => {
           if (err === null) {
@@ -28,13 +38,32 @@ function AtTheaters() {
     }
   };
 
-  useEffect(() => {
-    getXml();
-  }, []);
-
   return (
     <div className="atTheatersContainer">
-      <div className="results">
+      <div className="selectBar">
+        <select name="city" id="city" onChange={handleSelectChange}>
+          <option value="" className="other">Select a city</option>
+          <option value="1039" className="other">Espoo: OMENA</option>
+          <option value="1038" className="other">Espoo: SELLO</option>
+          <option value="1045" className="other">Helsinki: ITIS</option>
+          <option value="1031" className="other">Helsinki: KINOPALATSI</option>
+          <option value="1032" className="other">Helsinki: MAXIM</option>
+          <option value="1033" className="other">Helsinki: TENNISPALATSI</option>
+          <option value="1013" className="other">Vantaa: FLAMINGO</option>
+          <option value="1015" className="other">Jyväskylä: FANTASIA</option>
+          <option value="1016" className="other">Kuopio: SCALA</option>
+          <option value="1017" className="other">Lahti: KUVAPALATSI</option>
+          <option value="1041" className="other">Lappeenranta: STRAND</option>
+          <option value="1018" className="other">Oulu: PLAZA</option>
+          <option value="1019" className="other">Pori: PROMENADI</option>
+          <option value="1034" className="other">Tampere: CINE ATLAS</option>
+          <option value="1035" className="other">Tampere: PLEVNA</option>
+          <option value="1022" className="other">Turku: KINOPALATSI</option>
+          <option value="1046" className="other">Raisio: LUXE MYLLY</option>
+        </select>
+        <button type="button" onClick={handleButton}>Search</button>
+      </div>
+      <div className="atTheatersResults">
         <ul>
           {atThreaters}
         </ul>
